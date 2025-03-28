5dea02a20ab5b6da0617564b10abbdb24a636321
Brandon <brandoncrites76@gmail.com>
2025-03-28 07:05:09 -0700

    Homepage

--- Components/ReviewSlide.jsx
+++ Components/ReviewSlide.jsx
@@ -60,7 +60,7 @@ export default function TestimonialSlider() {
           <div
             key={i}
             onClick={() => setIndex(i)}
-            className={`w-3 h-3 rounded-full cursor-pointer ${
+            className={`w-2 h-2 rounded-full cursor-pointer ${
               index === i ? 'bg-black' : 'bg-gray-300'
             }`}
           ></div>
